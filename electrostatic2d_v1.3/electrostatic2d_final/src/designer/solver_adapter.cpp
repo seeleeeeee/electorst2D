4ff33875ce444853126c149ed0dcf31ff16e1a49
seeleeeeee <gunthervs2006@gmail.com>
2026-04-22 02:44:15 +0700

    Delete electrostatic2d_v1.3/electrostatic2d_final directory

--- electrostatic2d_v1.3/electrostatic2d_final/src/designer/solver_adapter.cpp
+++
@@ -1,5 +0,0 @@
-#include "solver_adapter.h"
-
-namespace electrostatic {
-// Все методы уже реализованы в .h файле
-}
--- electrostatic2d_v1.3/electrostatic2d_final/src/fem2d/geometryparser.cpp
+++
@@ -1,9 +0,0 @@
-#include "geometryparser.h"
-
-electrostatic::Geometry GeometryParser::parseString(const std::string& jsonText) const {
-    return electrostatic::Geometry();
-}
-
-electrostatic::Geometry GeometryParser::parseFile(const std::string& filePath) const {
-    return electrostatic::Geometry();
-}
